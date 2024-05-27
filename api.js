2aa0fce3b06b659e826262f7b211a98aa0dba5d9
jeremiasianigro <ianigroj@gmail.com>
2024-05-27 20:50:25 -0300

    hicimos funcionar la api

--- api.js
+++ api.js
@@ -1,28 +1,26 @@
+const url = 'https://imdb-top-100-movies.p.rapidapi.com/';
+const lista= document.querySelector("#movies-list");
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '70499d3174msh5c7bfbf026ccf27p114a63jsndaffe2639410',
+		'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+	}
+};
 
-let contenedorPersonajes = document.getElementById("personajes")
-
-fetch("https://rickandmortyapi.com/api/character")
-.then((response)=>response.json())
-.then((datos)=>{
-
-    console.log(datos)
-    console.log(datos.results)
-
-    datos.results.forEach((elementos) => {
-        // console.log(elementos.name)
-
-        const contenedorCreado = document.createElement('div')
-
-        contenedorCreado.innerHTML = `
-            <h4>${elementos.name}</h4>
-            <img src="${elementos.image}">
+fetch(url,options)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(movie =>{
+            const li = document.createElement("li");
+            li.innerHTML = `
+            <h4>${movie.title}</h4>
+            <p>${movie.description}</p>
+            <img src="${movie.image}">
+            
             
         `;
+            lista.append(li);
+        })
+    })
 
-
-       contenedorPersonajes.append(contenedorCreado)
-
-
-    });
-
-})
